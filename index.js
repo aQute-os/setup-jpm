7b35cc462651191ef750667dd77c954c0d6c2a66
Peter Kriens <Peter.Kriens@aqute.biz>
2021-12-02 18:00:52 +0100

    wrkflow

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const tc = require('@actions/tool-cache');
 
 async function setup() {
-  try {
-    await tc.downloadTool('https://repo1.maven.org/maven2/biz/aQute/bnd/biz.aQute.jpm.run/3.5.0/biz.aQute.jpm.run-3.5.0.jar', 'jpm.jar');
-    await exec.exec("java -jar jpm.jar init");
-  } catch (e) {
-    console.log(e);
-    core.setFailed(e);
-  }
+    console.log("hello world");
 }
 
 module.exports = setup
