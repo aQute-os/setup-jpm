d3791c3e514ef919ef0fe3248c022f39fab410d6
Peter Kriens <Peter.Kriens@aqute.biz>
2021-12-02 18:03:14 +0100

    wrkflow

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
 
 async function setup() {
-    console.log("hello world");
+  try {
+    await tc.downloadTool('https://repo1.maven.org/maven2/biz/aQute/bnd/biz.aQute.jpm.run/3.5.0/biz.aQute.jpm.run-3.5.0.jar', 'jpm.jar');
+    await exec.exec("java -jar jpm.jar init");
+  } catch (e) {
+    console.log(e);
+    core.setFailed(e);
+  }
 }
 
 module.exports = setup
