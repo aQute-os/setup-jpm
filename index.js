d933315f0fd6184d31b9c71a7ccb7e844e2a5d96
Peter Kriens <Peter.Kriens@aqute.biz>
2021-12-06 12:34:48 +0100

    Yet another

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const tc = require("@actions/tool-cache");
 const { formatWithOptions } = require("util");
-const path = require("path");
 
 const bndRemote =
   "https://github.com/pkriens/setup-bnd/raw/master/jar/biz.aQute.bnd.jar";
@@ -13,10 +12,10 @@ async function setup() {
     const bndLocal = await tc.downloadTool(bndRemote);
     await exec.exec(`java -jar ${bndLocal} version`);
     fs.mkdirSync(".bin");
-    if (path.delimiter == ";") {
-      fs.writeFileSync(".bin\\bnd.bat", `java -jar ${bndLocal} %*\n`);
-    }
-    fs.writeFileSync(".bin/bnd", `#!/bin/sh\njava -jar ${bndLocal} "$@"\n`);
+    fs.writeFileSync(
+      ".bin/bnd",
+      "#!/bin/sh\njava -jar " + bndLocal + ' "$@"\n'
+    );
     fs.chmodSync(".bin/bnd", 0o777);
 
     core.addPath(".bin/");
