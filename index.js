b632c987d0281e929aad94c1cb0658e606ce24bc
Peter Kriens <Peter.Kriens@aqute.biz>
2021-12-06 10:05:11 +0100

    Cleanup

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 const tc = require("@actions/tool-cache");
 
+const bndRemote =
+  "https://github.com/pkriens/setup-bnd/raw/master/jar/biz.aQute.bnd.jar";
+
 async function setup() {
   try {
     try {
-      fs.unlinkSync("jpm.jar");
+      fs.unlinkSync(bndLocal);
     } catch (e) {
-      console.log("oops " + e);
+      // ignore
     }
-    const jpm = await tc.downloadTool(
-      "https://repo1.maven.org/maven2/biz/aQute/bnd/biz.aQute.jpm.run/3.5.0/biz.aQute.jpm.run-3.5.0.jar",
-      "jpm.jar"
-    );
-    await exec.exec("java -jar jpm.jar -B bin init");
-    await exec.exec(
-      "bin/jpm install -f https://github.com/pkriens/setup-bnd/raw/master/jar/biz.aQute.bnd.jar"
-    );
-    core.addPath("bin");
+    const bndLocal = await tc.downloadTool(bndRemote);
+    await exec.exec(`java -jar ${bndLocal} buildtool`);
   } catch (e) {
     console.log(e);
     core.setFailed(e);
